a1a8e6f4a862bba2ceba6fc050e6b9a03b9a900f
xDriss <123965804+xDriss@users.noreply.github.com>
2023-02-24 22:26:19 +0100

    Update ms_rewards_farmer.py

--- ms_rewards_farmer.py
+++ ms_rewards_farmer.py
@@ -31,7 +31,8 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
-
+import os
+os.environ['PYTHONIOENCODING'] = 'utf-8'
 # Define user-agents
 PC_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.46'
 MOBILE_USER_AGENT = 'Mozilla/5.0 (Linux; Android 12; SM-N9750) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36 EdgA/110.0.1587.41'
